ac9de67809bc4239d8f9562c543d2fb11992c99e
-ly <perfect.ly.secret.ly@gmail.com>
2017-05-04 23:36:14 +0900

    test

--- main.js
+++ main.js
@@ -3,3 +3,10 @@ var storage = sessionStorage;
 storage.setItem('name', 'aaaa');
 
 var name = storage.getItem('name');
+window.addEventListener('storage', (e) => {
+    console.log(e.key); // key
+    console.log(e.oldValue); //古い値
+    console.log(e.newValue); //変更後の値
+    console.log(e.storageArea); //変化があった時の、storageのデータ全体をオブジェクトで取得
+    console.log(e.url); //オリジン
+});
